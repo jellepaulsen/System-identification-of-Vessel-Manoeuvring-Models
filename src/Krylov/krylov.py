c9d2c55bc23e21ceb6b187277b4ce4ce45789717
jellepaulsen <jelle.h.paulsen@stu.haw-kiel.de>
2026-06-09 11:04:57 +0200

    adding calc c3

--- src/Krylov/krylov.py
+++ src/Krylov/krylov.py
@@ -7,41 +7,43 @@ import yaml
 
 class Krylov_pre_calc:    
     def __init__(self, ship_parameters: dict, krylov_parameters: dict):
-        self.sparams = ship_parameters
-        self.kparams = krylov_parameters
-        self.eps = self.kparams["eps"]
-        self.psi2p = self.kparams["psi_2"] # psi_2 parameters
-        self.c2p = self.kparams["c_2"] # c_2 parameters
+        self.sp = ship_parameters
+        self.kp = krylov_parameters
+        self.eps = self.kp["eps"]
+        self.psi2p = self.kp["psi_2"] # psi_2 parameters
+        self.c2p = self.kp["c_2"] # c_2 parameters
+        self.cy_betap = self.kp["cy_beta"] # cy_beta parameters
 
-        self.cp = self.sparams["CB"]/self.sparams["CM"] # prismatic coefficient
+        self.cp = self.sp["CB"]/self.sp["CM"] # prismatic coefficient
 
-        self.rho = self.sparams["rho"]
-        self.L = self.scale_dim1D(self.sparams["L"])
-        self.B = self.scale_dim1D(self.sparams["B"])
-        self.Tm = self.scale_dim1D(self.sparams["Tm"])
-        # self.dbh = self.scale_dim1D(self.kparams["dbh"]) # currently unknown 
-        self.xtg = self.scale_dim1D(self.sparams['x_G'])/self.L # distance from centerline to bilge keel, scaled with ship scale factor
-        self.volume = self.L * self.B * self.Tm * self.sparams["CB"]
+        self.rho = self.sp["rho"]
+        self.L = self.scale_dim1D(self.sp["L"])
+        self.B = self.scale_dim1D(self.sp["B"])
+        self.Tm = self.scale_dim1D(self.sp["Tm"])
+        # self.dbh = self.scale_dim1D(self.kp["dbh"]) # currently unknown 
+        self.xtg = self.scale_dim1D(self.sp['x_G'])/self.L # distance from centerline to bilge keel, scaled with ship scale factor
+        self.volume = self.L * self.B * self.Tm * self.sp["CB"]
         self.m = self.volume * self.rho    #
-        self.xtg = self.sparams['x_G']/self.L
-        self.askeg = self.scale_dim2D(self.sparams["askeg"])
-        if self.L/self.B > 11:
+        self.xtg = self.sp['x_G']/self.L
+        self.askeg = self.scale_dim2D(self.sp["askeg"])
+        self.LB = self.L / self.B
+        if self.LB > 11:
             print("Krylov code only implemented for ships with L/B ratio less than 11")
             return
 
     def scale_dim1D(self, value):
-        return value * self.sparams["scale_factor"]
+        return value * self.sp["scale_factor"]
     def scale_dim2D(self, value):
-        return value * self.sparams["scale_factor"]**2
+        return value * self.sp["scale_factor"]**2
     def scale_dim3D(self, value):
-        return value * self.sparams["scale_factor"]**3
+        return value * self.sp["scale_factor"]**3
 
     def hydro_mass(self):
         # calculation of hydrodynamic mass using the Krylov code
-        R1_munk=self.kparams["R1_munk"]      
-        R2_munk=self.kparams["R2_munk"]        
-        R3_munk=self.kparams["R3_munk"]        
-        C=self.kparams["C"]	           #Lewis coefficient
+        R1_munk=self.kp["R1_munk"]      
+        R2_munk=self.kp["R2_munk"]        
+        R3_munk=self.kp["R3_munk"]        
+        C=self.kp["C"]	           #Lewis coefficient
 
         self.hydro_mass = {}
         
@@ -50,24 +52,24 @@ class Krylov_pre_calc:
         m66 = np.pi() * self.rho* self.T**2 * self.L**2. * C *R3_munk / 24  * self.L
         
 
-        if self.sparams["noh"] > 2:
+        if self.sp["noh"] > 2:
             print("Krylov code only implemented for monohulls and catamarans, not for trimarans or higher")
         
-        elif self.sparams["noh"] == 2:
+        elif self.sp["noh"] == 2:
             qqq = -1.0 + self.dbh / self.B
             Akxx = 2. + np.exp(-qqq)
             Akyy = 2. -0.8*np.exp(-2.*qqq)
             Corr_x=2.	
             Corr_y = 2.-0.5*np.exp(-2.*qqq)
             Corr_n=2.-0.65*np.exp(-2.*qqq)
-            m = self.sparams["CB"]*self.rho*self.L*self.sparams["B"]*self.T*2
-            volume = self.L*self.sparams["B"]*self.T*self.sparams["CB"]
+            m = self.sp["CB"]*self.rho*self.L*self.sp["B"]*self.T*2
+            volume = self.L*self.sp["B"]*self.T*self.sp["CB"]
             self.hydro_mass["m11"] = m11 * Akxx
             self.hydro_mass["m22"] = m22 * Akyy
             self.hydro_mass["m66"] = Akyy*(m66+((self.dbh/2.0)**2)*m22)+Akxx*((self.dbh/2.0)**2)*m11 
             self.hydro_mass["izz"] = 11115 # fixed value for catamaran no idea about dimension 
         else: 
-            volume = self.sparams["CB"] * self.L * self.sparams["B"] * self.T
+            volume = self.sp["CB"] * self.L * self.sp["B"] * self.T
             m = volume * self.rho
             C = 1.0
 
@@ -125,15 +127,15 @@ class Krylov_forces:
             x0 = self.x0_
         # If no initial values for ta and tf, take mean draft as inital value for both
         if ta or tf is None:
-            ta, tf = self.kpc.T, self.kpc.T 
+            ta, tf = self.kpc.Tm, self.kpc.Tm 
             # später kann es auch aus dem mittleren Tiefgang und dem Trim aus dem IMU berechnet werden zu Fahrtantritt
 
 
-        Uchar = np.sqrt(self.x0_[3]**2 + self.x0_[4]**2) # speed
+        Uchar = np.sqrt(x0[3]**2 + x0[4]**2) # speed
         self.Fn = Uchar / np.sqrt(self.kpc.L * 9.81) # Froude number
 
-        self.xtg = self.kpc.sparams['x_G']/self.kpc.L
-        self.psi1 = (ta- tf) / self.kpc.L # tangent ot static trim angle
+        self.xtg = self.kpc.sp['x_G']/self.kpc.L
+        self.psi1 = (ta - tf) / self.kpc.L # tangent ot static trim angle
 
 
         
@@ -167,11 +169,11 @@ class Krylov_forces:
         self.TmL = self.kpc.Tm / self.kpc.L
         askeg = self.kpc.askeg
         psi_res = self.psi1 + self.psi2
-        if self.kpc.sparams["shiptype"] == 1:
-            self.sigma =1.-(3./(20-i+0.000))*(askeg/(self.kpc.L*self.kpc.Tm))+(0.054/(self.TmL)) * psi_res
-        elif self.kpc.sparams["shiptype"] == 2:
+        if self.kpc.sp["shiptype"] == 1:
+            self.sigma = 1.-(3./(20.-self.kpc.sp["fr_i"]))*(askeg/(self.kpc.L*self.kpc.Tm))+(0.054/(self.TmL)) * psi_res
+        elif self.kpc.sp["shiptype"] == 2:
             self.sigma = 0.975 + 0.054/self.TmL * psi_res
-        elif self.kpc.sparams["shiptype"] == 3:
+        elif self.kpc.sp["shiptype"] == 3:
             self.sigma = 0.962 + 0.054/ self.TmL * psi_res
         
         # lower limit
@@ -200,8 +202,44 @@ class Krylov_forces:
         Q = a2 * (self.kpc.L/ self.kpc.B) + b2
         self.c2 = np.clip(a3 * Q + b3, 0.3, 1.6)
 
+    def calc_cy_beta(self):
+        for lb in self.kpc.cy_betap.values():
+            if lb["r"][0] <= self.kpc.LB <= lb["r"][1]:
+                for sigma in lb["sigma"].values():
+                    if sigma["r"][0] <= self.sigma <= sigma["r"][1]:
+                        a1 = self.coeffs(self.kpc.LB, *lb["a1"])
+                        b1 = self.coeffs(self.kpc.LB, *lb["b1"])
 
+        a2 = self.coeffs(self.TmL, 16.67, -11.92, 0.06)
+        b2 = self.coeffs(self.TmL, 261.1, 213.6, 2.468)
 
+        a3 = self.coeffs(self.kpc.cp, 0.2392, -0.4009, 0.1815)
+        b3 = self.coeffs(self.kpc.cp, 0.4033, -0.6965, 0.3263)
+
+        U = a1 * self.LB + b1
+        Q = a2 * U + b2
+
+        self.cy_beta = np.clip(a3 * Q + b3, 0.0, 0.5)
+
+    def calc_c3(self):
+        a2 = self.coeffs(self.TmL, 2.269, -0.5805, 0.00183)
+        b2 = self.coeffs(self.TmL, -27.7, 6.428, -0.01749)
+
+        if self.kpc.cp <= 0.72:
+            a1 = self.coeffs(self.kpc.cp, 24.65, -29.67, 7.547)
+        elif self.kpc.cp > 0.72:
+            a1 = self.coeffs(self.kpc.cp, 0, 5.917, 5.3)
+
+        if self.kpc.cp <= 0.68:
+            b1 = self.coeffs(self.kpc.cp, -60.44, 74.61, 9.255)
+        elif self.kpc.cp > 0.68:
+            b1 = self.coeffs(self.kpc.cp, 0, 10.08, 20.34)
+
+        U = a1 * self.LB + b1
+        self.c3 = np.clip(a2 * U + b2, 0.0, 0.35)
+
+         
+        
 if __name__ == "__main__":
     with open("conf/base/parameters/krylov.yml", "r") as f:
         krylov_parameters = yaml.safe_load(f)
@@ -215,7 +253,7 @@ if __name__ == "__main__":
     kf.Fn = 0.50
     kf.kpc.xtg = -0.03
     kf.calc_psi2()
-    kf.krylov_force()
+    kf.krylov_force([0,0,0, 2,1,0], ta=0.45, tf=0.40)
     kf.calc_sigma()
     print(f"result: {kf.sigma}")
 
